6cc20374032fde9f63eaf61d9acf3bdf1dcf2327
何阳 <heyang1014@163.com>
2022-07-19 11:34:08 +0800

    refactor(npm包): vue-goole-plugin开发

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vue-google-plugin
-基于vue3的谷歌插件模版
+# chrome-plugin
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/popup/components/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pop-content">
+    <div class="pop-header">
+      内容-------
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+.pop-content {
+  font-size: 10px !important;
+  width: 700px;
+  height: 500px;
+  padding: 10px;
+
+  .pop-header {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
+
